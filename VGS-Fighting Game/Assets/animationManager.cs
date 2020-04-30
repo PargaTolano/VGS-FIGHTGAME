f7b700a8c02b5e0669091f689167b8491b66b946
Jose Antonio Parga Tolano <parga.jose@outlook.com>
2020-04-29 22:10:17 -0500

    Initial Commit
    Se inicio el proyecto de Unity en la carpeta, partiendo de una ya existente

---
+++ VGS-Fighting Game/Assets/animationManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationManager : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("jab");
+        }
+
+        float axisV = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("move", axisV);
+    }
+}
